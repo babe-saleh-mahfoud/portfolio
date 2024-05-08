ea0f595abe2bf24b6a04153337c846a6678d8447
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-05-08 20:13:43 +0000

    Changes

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={`relative ${data.showCursor && "cursor-none"}`} style={{padding:16}}>
+    <div className={`relative ${data.showCursor && "cursor-none"}`} style={{padding:12}}>
       {data.showCursor && <Cursor />}
       <Head>
         <title>{data.name}</title>
